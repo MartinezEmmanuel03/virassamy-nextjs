412aa1167a1b8b27f61267b53620053d9b6aa8da
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2023-03-09 12:09:00 +0100

    mise en place envoi d'email via le formulaire de contact

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { ToastContainer, toast } from "react-toastify";
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/Layout'
@@ -14,6 +15,19 @@ import home from '../public/images/maison.png'
 import feedback from '../public/images/feedback.png'
 import mail from '../public/images/mail.png'
 
+import "react-toastify/dist/ReactToastify.css";
+
+const toastifyConfig = {
+  position: "top-right",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: false,
+  draggable: true,
+  progress: undefined,
+  theme: "light",
+};
+
 export default function Home({ presentation, statistiques, exclusivites, avis }) {
   const contactType = {
     personne: "",
@@ -21,14 +35,31 @@ export default function Home({ presentation, statistiques, exclusivites, avis })
     email: "",
     message: ""
   }
-  const [contact, setContact] = useState({ contactType })
+  const [contact, setContact] = useState(contactType)
 
   const handleContact = (place, value) => {
     const newContact = { ...contact };
     newContact[place] = value;
     setContact(newContact);
-    console.log(contact)
   };
+
+  const submitContact = async (e) => {
+    e.preventDefault();
+    await fetch('http://localhost:3000/api/contact', {
+      method: 'POST',
+      body: JSON.stringify({ contact }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(() => {
+        toast.success("Votre message a été envoyé", toastifyConfig);
+        setContact(contactType)
+      })
+      .catch((err) => {
+        toast.error("Une erreur s'est produite", toastifyConfig);
+      })
+  }
 
   return (
     <>
@@ -144,16 +175,55 @@ export default function Home({ presentation, statistiques, exclusivites, avis })
           {/* partie contact */}
           <div id='contact' className='py-8'>
             <h2 className='text-peach font-title text-5xl md:text-7xl text-center'>Contact</h2>
-            <form className='bg-lightbg mx-8 md:mx-12 mt-4 md:pt-12 lg:pt-16 py-8 rounded-xl text-center '>
+            <form
+              className='bg-lightbg mx-8 md:mx-12 mt-4 md:pt-12 lg:pt-16 py-8 rounded-xl text-center'
+              onSubmit={(e) => submitContact(e)}
+            >
               <div className='flex flex-col md:flex-row items-center md:mx-8 '>
                 <div className='flex flex-col md:h-40 md:justify-between items-center md:items-start w-full'>
-                  <input required type="text" placeholder="Nom et Prénom" name="personne" value={contact.personne} onChange={(e) => handleContact(e.target.name, e.target.value)} className='border border-peach w-3/4 md:w-4/5 mb-4 rounded-lg text-peach md:text-2xl pl-4' />
-                  <input required type="text" placeholder="Téléphone" name="telephone" value={contact.telephone} onChange={(e) => handleContact(e.target.name, e.target.value)} className='border border-peach w-3/4 md:w-4/5 mb-4 rounded-lg text-peach md:text-2xl pl-4' />
-                  <input required type="email" placeholder="Email" name="email" value={contact.email} onChange={(e) => handleContact(e.target.name, e.target.value)} className='border border-peach w-3/4 md:w-4/5 mb-4 md:mb-0 rounded-lg text-peach md:text-2xl pl-4' />
+                  <input
+                    required
+                    type="text"
+                    placeholder="Nom et Prénom"
+                    name="personne"
+                    value={contact.personne}
+                    onChange={(e) => handleContact(e.target.name, e.target.value)}
+                    className='border border-peach w-3/4 md:w-4/5 mb-4 rounded-lg text-peach md:text-2xl pl-4'
+                  />
+                  <input
+                    required
+                    type="text"
+                    placeholder="Téléphone"
+                    name="telephone"
+                    value={contact.telephone}
+                    onChange={(e) => handleContact(e.target.name, e.target.value)}
+                    className='border border-peach w-3/4 md:w-4/5 mb-4 rounded-lg text-peach md:text-2xl pl-4'
+                  />
+                  <input
+                    required
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    value={contact.email}
+                    onChange={(e) => handleContact(e.target.name, e.target.value)}
+                    className='border border-peach w-3/4 md:w-4/5 mb-4 md:mb-0 rounded-lg text-peach md:text-2xl pl-4'
+                  />
                 </div>
-                <textarea required type="text" row="5" placeholder="Message" name="message" value={contact.message} onChange={(e) => handleContact(e.target.name, e.target.value)} className='border border-peach w-3/4 md:w-4/5 h-40 rounded-lg text-peach md:text-2xl pl-4' />
+                <textarea
+                  required
+                  type="text"
+                  row="5"
+                  placeholder="Message"
+                  name="message"
+                  value={contact.message}
+                  onChange={(e) => handleContact(e.target.name, e.target.value)}
+                  className='border border-peach w-3/4 md:w-4/5 h-40 rounded-lg text-peach md:text-2xl pl-4'
+                />
               </div>
-              <button type="submit" className='bg-peach text-white rounded-xl mt-12 py-2 px-4 text-xl'>Envoyer</button>
+              <button
+                type="submit"
+                className='bg-peach text-white rounded-xl mt-12 py-2 px-4 text-xl'
+              >Envoyer</button>
             </form>
           </div>
           {/* tab bar */}
@@ -181,6 +251,18 @@ export default function Home({ presentation, statistiques, exclusivites, avis })
           </div>
         </main >
       </Layout>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   )
 }
